5db59fff911b0dddadb7ffa48596887d012dff17
Donvine <donvinemugendi@gmail.com>
2025-01-17 00:41:40 +0300

    Implement authentication layout and pages: add RootLayout with ClerkProvider, and create login and registration pages

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Course Loom",
+  description: "An LMS platform that powers the modern mind!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased scroll-smooth`}>
+        <ClerkProvider>{children}</ClerkProvider>
+      </body>
+    </html>
+  );
+}
