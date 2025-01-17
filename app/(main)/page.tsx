4d50da9e483f1b6fbda22b1bcc102b61a65d5aaa
Donvine <donvinemugendi@gmail.com>
2025-01-17 07:07:32 +0300

    update landing page design

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -11,7 +11,7 @@ import Image from "next/image";
 export default function Page() {
   return (
     <section className="bg-gradient-to-tr from-indigo-200 via-gray-100 to-indigo-200 ">
-      <div className="pt-12 bg-grid-indigo-100  min-h-screen  overflow-hidden py-2 md:pt-8 flex items-center justify-center relative">
+      <div className="pt-12 bg-grid-indigo-100 bg-opacity-20  min-h-screen  overflow-hidden py-2 md:pt-8 flex items-center justify-center relative">
         <div className="container mx-auto px-6 pt-12 md:pt-20 pb-16 md:pb-24 text-center max-w-4xl">
           <div className="inline-block">
             <span className="inline-flex items-center rounded-full px-4 py-1 text-sm bg-indigo-50 text-indigo-600 border border-indigo-600 mb-6">
