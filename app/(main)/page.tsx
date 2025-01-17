c9dc597a6db8c51b2ff3b535fd414c167c1c0f2d
Donvine <donvinemugendi@gmail.com>
2025-01-17 07:01:30 +0300

    update landing page design

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,13 +1,24 @@
-import { ArrowRight } from "lucide-react";
+import {
+  ArrowRight,
+  LibraryBig,
+  TrophyIcon,
+  UserRound,
+  Users,
+} from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
 export default function Page() {
   return (
-    <section className="bg-gradient-to-tr from-indigo-200 via-gray-50 to-indigo-200 ">
-      <div className="pt-12 bg-grid-indigo-100  min-h-screen  overflow-hidden py-2 md:pt-8 flex items-center justify-center ">
+    <section className="bg-gradient-to-tr from-indigo-200 via-gray-100 to-indigo-200 ">
+      <div className="pt-12 bg-grid-indigo-100  min-h-screen  overflow-hidden py-2 md:pt-8 flex items-center justify-center relative">
         <div className="container mx-auto px-6 pt-12 md:pt-20 pb-16 md:pb-24 text-center max-w-4xl">
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-gray-900 mb-6 md:mb-8">
+          <div className="inline-block">
+            <span className="inline-flex items-center rounded-full px-4 py-1 text-sm bg-indigo-50 text-indigo-600 border border-indigo-600 mb-6">
+              <TrophyIcon className="h-4 w-4 mr-2" /> Best E-Learning Services
+            </span>
+          </div>
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-gray-900 mb-6 md:mb-8 md:leading-loose tracking-tight">
             Unlock Your Potential with
             <span className="text-indigo-600 block md:inline">
               {" "}
@@ -19,10 +30,13 @@ export default function Page() {
             premium online courses. Start your learning journey today.
           </p>
           <div className="flex flex-col sm:flex-row items-center justify-center gap-4 px-4">
-            <button className="w-full sm:w-auto flex items-center justify-center bg-indigo-600 text-white px-8 py-3 rounded-full hover:bg-indigo-700 transition-colors">
+            <Link
+              className="w-full sm:w-auto flex items-center justify-center bg-indigo-600 text-white px-8 py-3 rounded-full hover:bg-indigo-700 transition-colors"
+              href="/dashboard"
+              prefetch={null}>
               Start Learning Now
               <ArrowRight className="ml-2 h-5 w-5 animate-move-arrow" />
-            </button>
+            </Link>
             <Link
               href="/courses"
               prefetch={null}
@@ -31,12 +45,68 @@ export default function Page() {
             </Link>
           </div>
         </div>
-        <Image
-          src="https://res.cloudinary.com/dipkbpinx/image/upload/v1737082617/illustrations/undraw_online-information_hhp2_x5mi1y.svg"
-          alt="illustration"
-          width={300}
-          height={300}
-        />
+        <div className="absolute -right-2 bottom-1/4 bg-white rounded-lg shadow-lg p-4 hidden lg:block">
+          <div className="flex items-center gap-2">
+            <Users className="w-5 h-5 text-indigo-600" />
+            <div>
+              <p className="font-bold">50+ Tutors</p>
+            </div>
+          </div>
+        </div>
+        <div className="absolute right-32 top-20 bg-white rounded-lg shadow-lg p-4 hidden lg:block">
+          <div className="flex items-center gap-2">
+            <LibraryBig className="w-5 h-5 text-indigo-600" />
+            <div>
+              <p className="font-bold">100+ Online Courses</p>
+            </div>
+          </div>
+        </div>
+        <div className="absolute -left-2 top-1/4 bg-white rounded-lg shadow-lg p-4 hidden lg:block">
+          <div className="text-center">
+            <div className="w-fit p-1 rounded-full text-white mx-auto bg-indigo-700">
+              <UserRound className="h-6 w-6" />
+            </div>
+            <p className="font-bold text-xl">15K+</p>
+            <p className="text-sm text-gray-600">Students Enrolled</p>
+            <div className="flex items-center -space-x-4">
+              <Image
+                alt="user 1"
+                src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80"
+                className="relative inline-block h-12 w-12 rounded-full border-2 border-white object-cover object-center hover:z-10 focus:z-10"
+                height={48}
+                width={48}
+              />
+              <Image
+                alt="user 2"
+                src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1061&amp;q=80"
+                className="relative inline-block h-12 w-12 rounded-full border-2 border-white object-cover object-center hover:z-10 focus:z-10"
+                height={48}
+                width={48}
+              />
+              <Image
+                alt="user 3"
+                src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1288&amp;q=80"
+                className="relative inline-block h-12 w-12 rounded-full border-2 border-white object-cover object-center hover:z-10 focus:z-10"
+                height={48}
+                width={48}
+              />
+              <Image
+                alt="user 4"
+                src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1287&amp;q=80"
+                className="relative inline-block h-12 w-12 rounded-full border-2 border-white object-cover object-center hover:z-10 focus:z-10"
+                height={48}
+                width={48}
+              />
+              <Image
+                alt="user 5"
+                src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1760&amp;q=80"
+                className="relative inline-block h-12 w-12 rounded-full border-2 border-white object-cover object-center hover:z-10 focus:z-10"
+                height={48}
+                width={48}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
