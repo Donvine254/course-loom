90d26e542a8da300dc5d6fa2e16e21151548bcc7
Donvine <donvinemugendi@gmail.com>
2025-01-20 22:49:53 +0300

    modify courses loading page

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from "next";
-// import CoursesPage from "./courses-page";
-import Loading from "./loading";
+import CoursesPage from "./courses-page";
 export const metadata: Metadata = {
   title: "Course Loom | Explore our courses",
   description: "An LMS platform that powers the modern mind!",
@@ -9,7 +8,7 @@ export const metadata: Metadata = {
 export default async function Courses() {
   return (
     <section className="py-2 pt-8 md:pt-10 bg-gradient-to-tr from-indigo-200 via-gray-100 to-indigo-200 dark:bg-gradient-to-tr dark:from-indigo-950 dark:via-gray-950 dark:to-indigo-950">
-      <Loading />
+      <CoursesPage />
     </section>
   );
 }
