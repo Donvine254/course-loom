28c8414a926dfa0fcc034c45156ebab8d937c6f4
Donvine <donvinemugendi@gmail.com>
2025-01-24 02:08:11 +0300

    add statistics and reasons sections to instructor landing page for enhanced engagement

--- app/(main)/teach/page.tsx
+++ app/(main)/teach/page.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 export default function Page() {
+  const stats = [
+    { value: "62M+", label: "Students" },
+    { value: "830M+", label: "Enrollments" },
+    { value: "180+", label: "Countries" },
+    { value: "75+", label: "Languages" },
+  ];
+
   return (
     <section className="py-2 pt-8 md:pt-10 bg-gradient-to-tr from-indigo-200 via-gray-100 to-indigo-200 dark:bg-gradient-to-tr dark:from-indigo-950 dark:via-gray-950 dark:to-indigo-950">
+      {/* Hero section */}
       <div className="min-h-screen">
         <div className="md:hidden flex flex-col">
           <div className="relative w-full h-[300px] sm:h-[400px]">
@@ -58,6 +67,73 @@ export default function Page() {
                 Get Started Today
               </Button>
             </div>
+          </div>
+        </div>
+      </div>
+      <Separator />
+      {/* Reasons section */}
+      <div className="py-4 pb-8 bg-background">
+        <div className="container mx-auto px-6">
+          <h2 className="text-4xl font-bold text-center mb-4 md:mb-8">
+            So many reasons to start
+          </h2>
+          <div className="grid md:grid-cols-3 gap-12">
+            <div className="text-center">
+              <Image
+                src="https://res.cloudinary.com/dipkbpinx/image/upload/v1737673025/illustrations/jkvb2et2tasqrhqt4abo.webp"
+                alt="teach-your-way"
+                height={80}
+                width={80}
+                className="w-20 h-20 bg-indigo-950  mx-auto mb-6 object-contain"
+              />
+
+              <h3 className="text-xl font-semibold mb-4">Teach your way</h3>
+              <p className="text-muted-foreground">
+                Publish the course you want, in the way you want, and always
+                have control of your own content.
+              </p>
+            </div>
+            <div className="text-center">
+              <Image
+                src="https://res.cloudinary.com/dipkbpinx/image/upload/v1737673025/illustrations/lgqsmy4pp0jwng7xssoa.webp"
+                alt="teach-your-way"
+                height={80}
+                width={80}
+                className="w-20 h-20 bg-indigo-950  mx-auto mb-6 object-contain"
+              />
+              <h3 className="text-xl font-semibold mb-4">Inspire learners</h3>
+              <p className="text-muted-foreground">
+                Teach what you know and help learners explore their interests,
+                gain new skills, and advance their careers.
+              </p>
+            </div>
+            <div className="text-center">
+              <Image
+                src="https://res.cloudinary.com/dipkbpinx/image/upload/v1737673024/illustrations/kqd3u8zjwbqhkydvqqvg.webp"
+                alt="teach-your-way"
+                height={80}
+                width={80}
+                className="w-20 h-20 bg-indigo-950  mx-auto mb-6 object-contain"
+              />
+              <h3 className="text-xl font-semibold mb-4">Get rewarded</h3>
+              <p className="text-muted-foreground">
+                Expand your professional network, build your expertise, and earn
+                money on each paid enrollment.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Stats Section */}
+      <div className="bg-indigo-700 py-16 text-white">
+        <div className="container mx-auto px-6">
+          <div className="grid md:grid-cols-4 gap-8">
+            {stats.map((stat, index) => (
+              <div key={index} className="text-center">
+                <div className="text-5xl font-bold mb-3">{stat.value}</div>
+                <div className="text-lg font-medium">{stat.label}</div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
