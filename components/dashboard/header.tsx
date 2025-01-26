a30c8ab98e2ce4cd01a53a6a0f41ba367ee6ca2c
Donvine <donvinemugendi@gmail.com>
2025-01-27 01:55:45 +0300

    refactor Header component; replace button with a link for student view and add notification bell with tooltip

--- components/dashboard/header.tsx
+++ components/dashboard/header.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { SidebarHeader, SidebarTrigger } from "../ui/sidebar";
 import { Separator } from "../ui/separator";
-import { Button } from "../ui/button";
-import { Search } from "lucide-react";
+import { Bell, Search } from "lucide-react";
 import { sessionUser } from "@/types";
 import { ThemeSwitch } from "./theme-switch";
+import Link from "next/link";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 export default function Header({ user }: { user?: sessionUser }) {
   return (
     <SidebarHeader className="fixed top-0 h-20 z-10 bg-white dark:bg-black transition-colors duration-300 w-full border-b border-input">
@@ -43,8 +49,28 @@ export default function Header({ user }: { user?: sessionUser }) {
         </div>
         {/* Right Section for student button */}
         <div className="flex md:static items-center gap-4 justify-start group-has-[[data-collapsible=icon]]/sidebar-wrapper:justify-center absolute right-0">
-          <Button variant="ghost">Student</Button>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger className="hover:bg-accent hover:text-accent-foreground inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none h-10 px-4 py-2">
+                <Link href="/learn">Student</Link>
+              </TooltipTrigger>
+              <TooltipContent
+                className="max-w-52"
+                side="bottom"
+                data-state="delayed-open">
+                <p>
+                  Switch to the student view here - get back to the courses
+                  you&apos;re taking.
+                </p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
           <ThemeSwitch className="xsm:hidden" />
+          <Link
+            href="/admin/user/notifications"
+            className="hover:bg-indigo-100 hover:text-indigo-500 dark:hover:bg-indigo-950 dark:hover:text-indigo-50 p-1 rounded-md">
+            <Bell className="h-5 w-5" />
+          </Link>
         </div>
       </div>
     </SidebarHeader>
